1d4eaae9d5f1151e868ec78372f81617e3af9e67
SunsetSamu <vsamuelalfredo@gmail.com>
2024-08-18 18:37:48 -0400

    update index.html and script.js

--- ves-to-usd/script.js
+++ ves-to-usd/script.js
@@ -4,48 +4,64 @@ document.getElementById('calculator-form').addEventListener('submit', calculate)
 let tasas = {};
 
 function updateRates() {
-    fetch("https://ve.dolarapi.com/v1/dolares/oficial")
-        .then(response => response.json())
-        .then(data => {
-            const ratesContainer = document.getElementById('rates');
-            ratesContainer.innerHTML = '';
+    Promise.all([
+        fetch("https://ve.dolarapi.com/v1/dolares/oficial").then(response => response.json()),
+        fetch("https://ve.dolarapi.com/v1/dolares/paralelo").then(response => response.json())
+    ])
+    .then(([ofic, para]) => {
+        const ratesContainer = document.getElementById('rates');
+        ratesContainer.innerHTML = '';
 
-            tasas = {
-                'Dólar Oficial (BCV)': data.promedio,
-            };
+        tasas = {
+            'Dólar Oficial (BCV)': ofic.promedio,
+            'Dólar Paralelo': para.promedio,
+        };
 
-            for (const [nombre, tasa] of Object.entries(tasas)) {
-                const rateElement = document.createElement('div');
-                rateElement.textContent = `${nombre}: ${tasa.toFixed(2)} VES`;
-                ratesContainer.appendChild(rateElement);
-            }
-        })
-        .catch(error => {
-            console.error('Error al obtener las tasas:', error);
-            document.getElementById('rates').textContent = 'Error al cargar las tasas. Intenta de nuevo más tarde.';
-        });
-}
+        for (const [nombre, tasa] of Object.entries(tasas)) {
+            const rateElement = document.createElement('div');
+            rateElement.textContent = `${nombre}: ${tasa.toFixed(2)} VES`;
+            ratesContainer.appendChild(rateElement);
+        }
+    })
+    .catch(error => {
+        console.error('Error al obtener las tasas:', error);
+        document.getElementById('rates').textContent = 'Error al cargar las tasas. Intenta de nuevo más tarde.';
+    });
+};
+
 
 function calculate(event) {
     event.preventDefault();
 
     const amount = parseFloat(document.getElementById('amount').value);
     const conversionType = document.getElementById('conversion-type').value;
-    let result = 0;
+    let result_of = 0;
+    let result_pa = 0;
 
-    // Utiliza la tasa del "Dólar Paralelo" para la conversión
-    const rate = tasas['Dólar Oficial (BCV)'];
+    // Utiliza la tasa del "Dólar oficial" para la conversión
+    const rate_of = tasas['Dólar Oficial (BCV)'];
 
     if (conversionType === 'ves-to-usd') {
         // Convierte de VES a USD
-        result = amount / rate;
+        result_of = amount / rate_of;
     } else if (conversionType === 'usd-to-ves') {
         // Convierte de USD a VES
-        result = amount * rate;
+        result_of = amount * rate_of;
+    }
+
+    // Utiliza la tasa del "Dólar paralelo" para la conversión
+    const rate_pa = tasas['Dólar Paralelo'];
+
+    if (conversionType === 'ves-to-usd') {
+        // Convierte de VES a USD
+        result_pa = amount / rate_pa;
+    } else if (conversionType === 'usd-to-ves') {
+        // Convierte de USD a VES
+        result_pa = amount * rate_pa;
     }
 
     // Muestra el resultado
-    document.getElementById('result').textContent = `Resultado: ${result.toFixed(2)} ${conversionType === 'ves-to-usd' ? 'USD' : 'VES'}`;
+    document.getElementById('result').textContent = `Resultado al Oficial: ${result_of.toFixed(2)} ${conversionType === 'ves-to-usd' ? 'USD' : 'VES'} | Resultado al Paralelo: ${result_pa.toFixed(2)} ${conversionType === 'ves-to-usd' ? 'USD' : 'VES'}`;
 }
 
 // Llamada inicial para cargar las tasas cuando se carga la página
