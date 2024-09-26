c5bd9ae47d4bb74fa52a0e29e4fd1f46e983b865
SunsetSamu <vsamuelalfredo@gmail.com>
2024-09-26 00:39:38 -0400

    Remade Index + Fixed USD to VES

--- ves-to-usd/script.js
+++ ves-to-usd/script.js
@@ -1,6 +1,13 @@
 document.getElementById('update-button').addEventListener('click', updateRates);
 document.getElementById('calculator-form').addEventListener('submit', calculate);
 
+const OFICIAL='https://ve.dolarapi.com/v1/dolares/oficial';
+const PARALELO='https://ve.dolarapi.com/v1/dolares/paralelo';
+
+
+
+
+// Script para desactivar el scroll horizontal 
 $(function() {
     var $body = $(document);
     $body.bind('scroll', function() {
@@ -12,69 +19,69 @@ $(function() {
   });
   
 
-let tasas = {};
+var tasaOficial = 0;
+var tasaParalelo = 0;
 
-function updateRates() {
-    Promise.all([
-        fetch("https://ve.dolarapi.com/v1/dolares/oficial").then(response => response.json()),
-        fetch("https://ve.dolarapi.com/v1/dolares/paralelo").then(response => response.json())
-    ])
-    .then(([ofic, para]) => {
-        const ratesContainer = document.getElementById('rates');
-        ratesContainer.innerHTML = '';
+function updateRates(event) {
+    console.log('updateRates() called');
+    fetch(OFICIAL)
+    .then(response => response.json())
+    .then(data => {
+        console.log('data of OFICIAL API:', data);
+        console.log('tasa of', data.promedio);
 
-        tasas = {
-            'Dólar Oficial (BCV)': ofic.promedio,
-            'Dólar Paralelo': para.promedio,
-        };
-
-        for (const [nombre, tasa] of Object.entries(tasas)) {
-            const rateElement = document.createElement('div');
-            rateElement.textContent = `${nombre}: ${tasa.toFixed(2)} VES`;
-            ratesContainer.appendChild(rateElement);
-        }
-    })
-    .catch(error => {
-        console.error('Error al obtener las tasas:', error);
-        document.getElementById('rates').textContent = 'Error al cargar las tasas. Intenta de nuevo más tarde.';
+        tasaOficial = Number(data.promedio);
+        console.log('tasa of', tasaOficial);
     });
-};
 
+    fetch(PARALELO)
+    .then(response => response.json())
+    .then(data => {
+        console.log('data of PARALELO API:', data);
+        console.log('tasa pa', data.promedio);
 
+        tasaParalelo = Number(data.promedio);
+        console.log('tasa pa', tasaParalelo);
+    });
+
+    
+    document.getElementById('rates').innerHTML = `
+        <p>
+            Cargando...
+        </p>
+    `;
+    setTimeout(() => {
+        document.getElementById('rates').innerHTML = `
+            <p>
+                D&oacute;lar BCV: ${tasaOficial}
+            </p>
+            <p>
+                D&oacute;lar Paralelo: ${tasaParalelo}
+            </p>
+        `;
+    }, 2000);
+}
 function calculate(event) {
     event.preventDefault();
 
     const amount = parseFloat(document.getElementById('amount').value);
     const conversionType = document.getElementById('conversion-type').value;
-    let result_of = 0;
-    let result_pa = 0;
+    const rate_of = tasaOficial;
+    const rate_pa = tasaParalelo;
 
-    // Utiliza la tasa del "Dólar oficial" para la conversión
-    const rate_of = tasas['Dólar Oficial (BCV)'];
+    let result_of = 0, result_pa = 0;
 
     if (conversionType === 'ves-to-usd') {
-        // Convierte de VES a USD
         result_of = amount / rate_of;
-    } else if (conversionType === 'usd-to-ves') {
-        // Convierte de USD a VES
-        result_of = amount * rate_of;
-    }
-
-    // Utiliza la tasa del "Dólar paralelo" para la conversión
-    const rate_pa = tasas['Dólar Paralelo'];
-
-    if (conversionType === 'ves-to-usd') {
-        // Convierte de VES a USD
         result_pa = amount / rate_pa;
-    } else if (conversionType === 'usd-to-ves') {
-        // Convierte de USD a VES
+    } else {
+        result_of = amount * rate_of;
         result_pa = amount * rate_pa;
     }
 
-    // Muestra el resultado
     document.getElementById('result').innerHTML = `Resultado al Oficial: ${result_of.toFixed(2)} ${conversionType === 'ves-to-usd' ? 'USD' : 'VES'}<br>Resultado al Paralelo: ${result_pa.toFixed(2)} ${conversionType === 'ves-to-usd' ? 'USD' : 'VES'}`;
-
 }
+
 
 // Llamada inicial para cargar las tasas cuando se carga la página
 updateRates();
