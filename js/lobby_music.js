8f48e031c95b2103534885aa65911d46c67b94d9
SunsetSamu <vsamuelalfredo@gmail.com>
2025-11-23 21:55:36 -0400

    Add volume control slider for the lobby music

--- js/lobby_music.js
+++ js/lobby_music.js
@@ -1,35 +1,66 @@
 const music = new Audio('../assets/elements/untitled_3 v2.mp3');
 music.loop = true;
 
+const volumeSlider = document.getElementById('volumeSlider');
 const toggleBtn = document.getElementById('lobby_music_toggle');
 const icons = toggleBtn.querySelectorAll('.music_button');
 
 
 let musicState = JSON.parse(localStorage.getItem('musicState')) || {
     playing: false,
-    currentTime: 0
+    currentTime: 0,
+    volume: 0.3
 };
 
-console.log(musicState.playing)
+console.log('Is Playing:', musicState.playing)
 music.currentTime = musicState.currentTime;
+music.volume = musicState.volume;
+volumeSlider.value = music.volume * 100;
 
+volumeSlider.addEventListener('input', function() {
+    music.volume = this.value / 100;
+});
+
+function updateButtonState() {
+    const isPlaying = !music.paused;
+    
+    icons.forEach((icon, index) => {
+        if ((isPlaying && index === 1) || (!isPlaying && index === 0)) {
+            icon.classList.add('active');
+            volumeSlider.classList.add('active');
+        } else {
+            icon.classList.remove('active');
+            volumeSlider.classList.remove('active')
+        }
+    });
+
+
+}
+
+updateButtonState();
 
 if (musicState.playing) {
-    music.play().catch
+    music.play().then(() => {
+        updateButtonState();
+    })
+    .catch(error => {
+        console.log('Autoplay bloqueado:', error);
+        musicState.playing = false;
+        saveAudioState();
+        updateButtonState();
+    });
 };
 
 toggleBtn.addEventListener('click', async function() {
-    icons.forEach(icon => {
-        icon.classList.toggle('active');
-    });
-    
     try {
         if (music.paused) {
             await music.play();
             console.log('Audio iniciado');
+            updateButtonState();
         } else {
             music.pause();
             console.log('Audio pausado');
+            updateButtonState();
         }
         saveAudioState();
 
@@ -38,14 +69,16 @@ toggleBtn.addEventListener('click', async function() {
 
         musicState.playing = false;
         saveAudioState();
+        updateButtonState();
 
     }
 });
 
 function saveAudioState() {
     musicState = {
-        playing: music.paused,
-        currentTime: music.currentTime
+        playing: !music.paused,
+        currentTime: music.currentTime,
+        volume: music.volume  
     };
     localStorage.setItem('musicState', JSON.stringify(musicState));
 }
